1a17f480b9e457d9b724a744217e626452d3057c
Max Carlquist <tenkaklet@gmail.com>
2016-04-28 18:40:37 +0200

    made chat, too easy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,28 @@ $(function () {
   //firebase
   var firebaseRef = new Firebase('https://ffl-chat.firebaseio.com/');
   console.log(firebaseRef);
+  var Login = $('#Login');
+    Login.click(function() {
+      console.log('hello');
+      firebaseRef.authWithOAuthPopup('github', function (error, authData) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(authData);
+        }
+      });
+    });
 
+  firebaseRef.onAuth(function (authData) {
+    if (authData) {
+      initChat(authData);
+    }
+  });
+  function initChat(authData) {
+  var chat = new FirechatUI(firebaseRef, document.getElementById('chatBox'));
+  chat.setUser(authData.uid, authData[authData.provider].username);
+  $('#userName').text(authData.github.username);
+}
 
 
 }); // End of the line jQuery
