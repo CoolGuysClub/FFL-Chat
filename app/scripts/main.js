d4a0f3b51e04a8d09b749260fc27cc757d64d8da
Max Carlquist <tenkaklet@gmail.com>
2016-04-28 18:01:47 +0200

    introduced firebase

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
 console.log('\'Allo \'Allo!');
+
+$(function () {
+
+  //firebase
+  var firebaseRef = new Firebase('https://ffl-chat.firebaseio.com/');
+  console.log(firebaseRef);
+
+
+
+}); // End of the line jQuery
